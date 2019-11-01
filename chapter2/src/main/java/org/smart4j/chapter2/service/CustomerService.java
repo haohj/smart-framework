1ba0cc59f9a42db0280d754f79bc61789fcf4003
郝红剑 <hongjian.hao@zjtzsw.com>
2019-11-01 10:13:09 +0800

    chapter2

---
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -0,0 +1,51 @@
+package org.smart4j.chapter2.service;
+
+import org.smart4j.chapter2.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 提供客户数据服务
+ */
+public class CustomerService {
+    /**
+     * 获取客户列表
+     */
+    public List<Customer> getCustomerList(String keyword) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * 获取客户
+     */
+    public Customer getCustomer(long id) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * 创建客户
+     */
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        //TODO
+        return false;
+    }
+
+    /**
+     * 更新客户
+     */
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+        //TODO
+        return false;
+    }
+
+    /**
+     * 删除客户
+     */
+    public boolean deleteCustomer(long id) {
+        //TODO
+        return false;
+    }
+}
