acda0d7e6b735b2551803b422ce61905b48d7a84
郝红剑 <hongjian.hao@zjtzsw.com>
2019-11-01 17:22:19 +0800

    工具类

--- chapter2/src/main/java/org/smart4j/chapter2/utils/StringUtil.java
+++ chapter2/src/main/java/org/smart4j/chapter2/utils/StringUtil.java
@@ -1,7 +1,25 @@
 package org.smart4j.chapter2.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * 字符串工具类
  */
 public class StringUtil {
+    /**
+     * 判断字符串是否为空
+     */
+    public static boolean isEmpty(String str) {
+        if (str != null) {
+            str = str.trim();
+        }
+        return StringUtils.isEmpty(str);
+    }
+
+    /**
+     * 判断字符串是否非空
+     * */
+    public static boolean isNotEmpty(String str){
+        return !isEmpty(str);
+    }
 }
