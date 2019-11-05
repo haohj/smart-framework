d10fbdcf45b49d8c36eb394f11dcb495a2a18096
郝红剑 <hongjian.hao@zjtzsw.com>
2019-11-05 14:55:11 +0800

    数据库操作助手类

--- chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -4,9 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smart4j.chapter2.helper.DatabaseHelper;
 import org.smart4j.chapter2.model.Customer;
-import org.smart4j.chapter2.utils.CastUtil;
 
-import java.sql.*;
 import java.util.List;
 import java.util.Map;
 
@@ -28,103 +26,28 @@ public class CustomerService {
      * 获取客户
      */
     public Customer getCustomer(long id) {
-        Connection conn = null;
-        try {
-            String sql = "select * from customer where id=?";
-            conn = DatabaseHelper.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setLong(1, id);
-            ResultSet rs = stmt.executeQuery();
-            Customer customer = new Customer();
-            while (rs.next()) {
-                customer.setId(id);
-                customer.setName(rs.getString("name"));
-                customer.setContact(rs.getString("contact"));
-                customer.setTelphone(rs.getString("telphone"));
-                customer.setEmail(rs.getString("email"));
-                customer.setRemark(rs.getString("remark"));
-            }
-            return customer;
-        } catch (SQLException e) {
-            log.error("execute sql failure", e);
-        } finally {
-            if (conn != null) {
-                DatabaseHelper.closeConnection();
-            }
-        }
-        return null;
+        String sql = "select * from customer where id=?";
+        return DatabaseHelper.queryEntity(Customer.class, sql, id);
     }
 
     /**
      * 创建客户
      */
     public boolean createCustomer(Map<String, Object> fieldMap) {
-        Connection conn = null;
-        try {
-            String sql = "insert into customer(id,name,contact,telphone,email,remark) values (?,?,?,?,?,?)";
-            conn = DatabaseHelper.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setLong(1, CastUtil.castLong(fieldMap.get("id")));
-            stmt.setString(2, CastUtil.castString(fieldMap.get("name")));
-            stmt.setString(3, CastUtil.castString(fieldMap.get("contact")));
-            stmt.setString(4, CastUtil.castString(fieldMap.get("telphone")));
-            stmt.setString(5, CastUtil.castString(fieldMap.get("email")));
-            stmt.setString(6, CastUtil.castString(fieldMap.get("remark")));
-            return stmt.execute(sql);
-        } catch (SQLException e) {
-            log.error("execute sql failure", e);
-        } finally {
-            if (conn != null) {
-                DatabaseHelper.closeConnection();
-            }
-        }
-        return false;
+        return DatabaseHelper.insertEntity(Customer.class, fieldMap);
     }
 
     /**
      * 更新客户
      */
     public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
-        Connection conn = null;
-        try {
-            String sql = "update customer set name=?,contact=?,telphone=?,email=?,remark=? where id=?";
-            conn = DatabaseHelper.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, CastUtil.castString(fieldMap.get("name")));
-            stmt.setString(2, CastUtil.castString(fieldMap.get("contact")));
-            stmt.setString(3, CastUtil.castString(fieldMap.get("telphone")));
-            stmt.setString(4, CastUtil.castString(fieldMap.get("email")));
-            stmt.setString(5, CastUtil.castString(fieldMap.get("remark")));
-            stmt.setLong(6, id);
-            return stmt.execute(sql);
-        } catch (SQLException e) {
-            log.error("execute sql failure", e);
-        } finally {
-            if (conn != null) {
-                DatabaseHelper.closeConnection();
-            }
-        }
-        return false;
+        return DatabaseHelper.updateEntity(Customer.class, id, fieldMap);
     }
 
     /**
      * 删除客户
      */
     public boolean deleteCustomer(long id) {
-        Connection conn = null;
-        try {
-            String sql = "delete from customer where id=?";
-            conn = DatabaseHelper.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setLong(1, id);
-            return stmt.execute(sql);
-        } catch (SQLException e) {
-            log.error("execute sql failure", e);
-        } finally {
-            if (conn != null) {
-                DatabaseHelper.closeConnection();
-            }
-        }
-        return false;
+        return DatabaseHelper.deleteEntity(Customer.class, id);
     }
 }
