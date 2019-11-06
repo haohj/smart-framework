28031f2adeefbdd51472a3a70c5940866a10bb96
郝红剑 <hongjian.hao@zjtzsw.com>
2019-11-06 13:58:23 +0800

    项目初始化

---
+++ smart-framework/src/main/java/org/smart4j/framework/utils/ClassUtil.java
@@ -0,0 +1,34 @@
+package org.smart4j.framework.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+/**
+ * 类操作工具类
+ */
+public class ClassUtil {
+    private static final Logger log = LoggerFactory.getLogger(ClassUtil.class);
+
+    /**
+     * 获取类加载器
+     */
+    public static ClassLoader getClassLoader() {
+        return null;
+    }
+
+    /**
+     * 加载类
+     */
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+        return null;
+    }
+
+    /**
+     * 获取指定包名下的所有类
+     */
+    public static Set<Class<?>> getClassSet(String packageName) {
+        return null;
+    }
+}
