1dff3123c3b797f04648ef26db6c1baf5b7b5834
郝红剑 <hongjian.hao@zjtzsw.com>
2019-11-12 11:08:19 +0800

    类操作助手类

---
+++ smart-framework/src/main/java/org/smart4j/framework/helper/ClassHelper.java
@@ -0,0 +1,66 @@
+package org.smart4j.framework.helper;
+
+import org.smart4j.framework.annotation.Controller;
+import org.smart4j.framework.annotation.Service;
+import org.smart4j.framework.utils.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 类操作助手类
+ */
+public class ClassHelper {
+    /**
+     * 定义类集合（用于存放所加载的类）
+     */
+    public static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    /**
+     * 获取应用包名下的所有类
+     */
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    /**
+     * 获取应用包名下的所有Service类
+     */
+    public static Set<Class<?>> getServiceClassSet() {
+        Set<Class<?>> classSet = new HashSet<>();
+        CLASS_SET.forEach((value) -> {
+            if (value.isAnnotationPresent(Service.class)) {
+                classSet.add(value);
+            }
+        });
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下的所有Controller类
+     */
+    public static Set<Class<?>> getControllerClassSet() {
+        Set<Class<?>> classSet = new HashSet<>();
+        CLASS_SET.forEach((value) -> {
+            if (value.isAnnotationPresent(Controller.class)) {
+                classSet.add(value);
+            }
+        });
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下的所有Bean类（包括Controller、Service）
+     */
+    public static Set<Class<?>> getBeanClassSet() {
+        Set<Class<?>> beanClassSet = new HashSet<>();
+        beanClassSet.addAll(getServiceClassSet());
+        beanClassSet.addAll(getControllerClassSet());
+        return beanClassSet;
+    }
+}
