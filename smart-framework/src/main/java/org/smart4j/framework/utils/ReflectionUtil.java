4797e2cbb24238f963c475e04450efe26366b919
郝红剑 <hongjian.hao@zjtzsw.com>
2019-11-12 11:33:11 +0800

    反射工具类

---
+++ smart-framework/src/main/java/org/smart4j/framework/utils/ReflectionUtil.java
@@ -0,0 +1,55 @@
+package org.smart4j.framework.utils;
+
+import org.apache.log4j.Logger;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * 反射工具类
+ */
+public class ReflectionUtil {
+    public static final Logger log = Logger.getLogger(ReflectionUtil.class);
+
+    /**
+     * 创建实例
+     */
+    public static Object newInstance(Class<?> cls) {
+        Object instance;
+        try {
+            instance = cls.newInstance();
+        } catch (Exception e) {
+            log.error("new instance failure", e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+
+    /**
+     * 调用方法
+     */
+    public static Object invokeMethod(Object obj, Method method, Object... args) {
+        Object result;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(obj, args);
+        } catch (Exception e) {
+            log.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * 设置成员变量值
+     */
+    public static void setField(Object obj, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+        } catch (Exception e) {
+            log.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
