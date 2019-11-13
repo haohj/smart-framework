8f4905338414beba488dec23805629fc1e9dc092
郝红剑 <hongjian.hao@zjtzsw.com>
2019-11-13 09:07:48 +0800

    数组工具类

--- smart-framework/src/main/java/org/smart4j/framework/utils/ArrayUtil.java
+++ smart-framework/src/main/java/org/smart4j/framework/utils/ArrayUtil.java
@@ -1,7 +1,22 @@
 package org.smart4j.framework.utils;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 /**
  * 数组工具类
  */
 public class ArrayUtil {
+    /**
+     * 判断数组是否非空
+     */
+    public static boolean isNotEmpty(Object[] objects) {
+        return !isEmpty(objects);
+    }
+
+    /**
+     * 判断数组是否为空
+     */
+    public static boolean isEmpty(Object[] objects) {
+        return ArrayUtils.isEmpty(objects);
+    }
 }
