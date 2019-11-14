e2dc3e65862264ce057fa331f59660f392f432e1
郝红剑 <hongjian.hao@zjtzsw.com>
2019-11-14 11:14:04 +0800

    编码与解码操作工具类

---
+++ smart-framework/src/main/java/org/smart4j/framework/utils/CodecUtil.java
@@ -0,0 +1,41 @@
+package org.smart4j.framework.utils;
+
+import org.apache.log4j.Logger;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+/**
+ * 编码与解码操作工具类
+ */
+public class CodecUtil {
+    private static final Logger log = Logger.getLogger(CodecUtil.class);
+
+    /**
+     * 将URL编码
+     */
+    public static String encodeURL(String source) {
+        String target;
+        try {
+            target = URLEncoder.encode(source, "UTF-8");
+        } catch (Exception e) {
+            log.error("encode url failure", e);
+            throw new RuntimeException(e);
+        }
+        return target;
+    }
+
+    /**
+     * 将URL解码
+     */
+    public static String decodeURL(String source) {
+        String target;
+        try {
+            target = URLDecoder.decode(source, "UTF-8");
+        } catch (Exception e) {
+            log.error("decode url failure", e);
+            throw new RuntimeException(e);
+        }
+        return target;
+    }
+}
